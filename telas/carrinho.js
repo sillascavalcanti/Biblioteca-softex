8acaf838e5630c8e6835a7aec0cca9a22826cc38
KarolynneFreire <karolynnefreire@gmail.com>
2024-07-17 18:29:40 -0300

    incluindo as funções remover e visualizar em carrinho.js

--- telas/carrinho.js
+++ telas/carrinho.js
@@ -12,7 +12,24 @@ function adicionarLivro() {
     carrinhoDeLivros.push(novoLivro);
     console.log(`Livro "${titulo}" adicionado ao carrinho.`);
 }
- 
+
+function removerLivro() {
+    let titulo = prompt("Qual livro você deseja remover?");
+    let indice = carrinhoDeLivros.findIndex(livro => livro.titulo === titulo);
+    if (indice !== -1) {
+        let livroRemovido = carrinhoDeLivros.splice(indice, 1)[0];
+        console.log(`Livro "${livroRemovido.titulo}" removido do carrinho.`);
+    } else {
+        console.log(`Livro "${titulo}" não encontrado no carrinho.`);
+    }
+}
+
+function visualizarCarrinho() {
+    console.log("Livros no carrinho:");
+    carrinhoDeLivros.forEach(livro => {
+        console.log(`- "${livro.titulo}" de ${livro.autor}`);
+    });
+}
 while (true) {
     let acao = prompt("O que você deseja fazer? (adicionar / remover / visualizar / sair)");
 
