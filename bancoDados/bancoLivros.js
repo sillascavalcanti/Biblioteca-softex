fbe8102f885ecec5016cdf8c0deb0e753ab430fb
CassianoChagasJr <cassianojrchagas@gmail.com>
2024-07-17 10:18:40 -0300

    p3 - Banco de Livros

---
+++ bancoDados/bancoLivros.js
@@ -0,0 +1,105 @@
+
+let listaLivros =[
+    {
+        titulo: 'It, A Coisa',
+        autor: 'Stephen King',
+        genero: 'Terror', 
+    },
+    {
+        titulo: 'O Corvo e Outras Histórias',
+        autor: 'Edgar Allan Poe',
+        genero: 'Terror',
+    },
+    {
+        titulo: 'O Chamado de Cthulhu e outras histórias',
+        autor: 'H.P Lovecraft',
+        genero: 'Terror',
+    },
+    {
+        titulo: 'Drácula',
+        autor: 'Bram Stoker',
+        genero: 'Terror',
+    },
+    {
+        titulo: 'Cemitério Maldito',
+        autor: 'Stephen King',
+        genero: 'Terror',
+    },
+    {
+        titulo: 'Duna',
+        autor: 'Frank Herbert',
+        genero: 'Fantasia',
+    },
+    {
+        titulo: 'O Lar Da Srta. Peregrine para Crianças Peculiares',
+        autor: 'Ransom Riggs',
+        genero: 'Fantasia',
+    },
+    {
+        titulo: 'O Hobbit',
+        autor: 'J. R. R. Tolkien',
+        genero: 'Fantasia',
+    },
+    {
+        titulo: 'A Batalha do Apocalipes',
+        autor: 'Eduardo Spohr',
+        genero: 'Fantasia',
+    },
+    {
+        titulo: 'Coraline',
+        autor: 'Neil Gaiman',
+        genero: 'Fantasia',
+    },
+    {
+        titulo: 'A Culpa é das Estrelas',
+        autor: 'John Green',
+        genero: 'Romance',
+    },
+    {
+        titulo: 'Como Eu Era Antes de Você ',
+        autor: 'Jojo Moyes',
+        genero: 'Romance',
+    },
+    {
+        titulo: 'Água para Elefantes',
+        autor: 'Sara Gruen',
+        genero: 'Romance',
+    },
+    {
+        titulo: 'O Morro Dos Ventos Uivantes',
+        autor: 'Emily Brontë (Ellis Bell)',
+        genero: 'Romance',
+    },
+    {
+        titulo: 'Um Dia',
+        autor: 'David Nicholls',
+        genero: 'Romance',
+    },
+    {
+        titulo: 'Memórias Póstumas de Brás Cubas',
+        autor: 'Machado de Assis',
+        genero: 'Nacionais',
+    },
+    {
+        titulo: 'Macunaíma',
+        autor: 'Mario de Andrade',
+        genero: 'Nacionais',
+    },
+    {
+        titulo: 'O Cortiço',
+        autor: 'Aluísio Azevedo',
+        genero: 'Nacionais',
+    },
+    {
+        titulo: 'Capitães de Areia',
+        autor: 'Jorge Amado',
+        genero: 'Nacionais',
+    },
+    {
+        titulo: 'Mar Absuoluto',
+        autor: 'Cecília Meireles',
+        genero: 'Nacionais',
+    }
+]
+
+console.log(listaLivros[1]);
