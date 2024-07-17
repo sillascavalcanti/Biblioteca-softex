f66d8dfb26ee26b35dd8626753477b312124d163
Vinicius Simas <viniciussimaslima19@gmail.com>
2024-07-16 22:32:20 -0300

    p1 - Boas Vindas

--- boasVindas.js
+++ boasVindas.js
@@ -1,12 +1,14 @@
-const divisoria = require('./elementosGraficos/divisoria');
+//Importações
 
+const divisoria = require('./elementosGraficos/divisoria');
+//Mensagem de Boas Vindas
 const boasVindas = { 
 
     mostrarTela: function() {
         divisoria()
-        console.log(                Seja bem vindo à Aquela Biblioteca);
+        console.log('              Seja bem vindo à Aquela Biblioteca');
         divisoria()
     }
 }
-
+//Exportações
 module.exports = boasVindas;
