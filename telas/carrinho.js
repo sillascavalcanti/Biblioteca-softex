c5fc86bbbd88327f778dcf54313ede535033ff8f
danielsantos404 <dan.danielfs@gmail.com>
2024-07-18 11:12:59 -0300

    p4 - Carrinho

--- telas/carrinho.js
+++ telas/carrinho.js
@@ -1,6 +1,7 @@
 const prompt = require("prompt-sync")({sigint:true});
 const listaLivros = require("../bancoDados/bancoLivros");
 const divisoria = require("../elementosGraficos/divisoria")
+const limparCarrinho = require("../telas/limparCarrinho")
 
 const listaDeLivros = listaLivros
 
@@ -9,74 +10,81 @@ const carrinho = {
         let carrinho = [];
 
         while (true) {
-            
+
             console.log(`              Meu Carrinho`);
-            console.log(` 1 → Adicionar Livro(s)`);
-            console.log(` 2 → Remoção de Livro(s)`);
-            console.log(` 3 → Visualizar Livro(s)`);
-            console.log(` 0 → Sair`);
-            divisoria();
-            let acao = prompt("O que você deseja fazer?");
+            visualizarCarrinho()
+            divisoria()
             
+            console.log(` 1 → Remover o primeiro Livro`);
+            console.log(` 2 → Remover o segundo Livro`);
+            console.log(` 3 → Remover o terceiro Livro`);
+            console.log(` 8 → Adicionar mais um Livro`);
+            console.log(` 9 → Finalizar Carrinho`);
+            console.log(` 0 → Limpar Carrinho`);
+            divisoria();
+            let acao = prompt("O que você deseja fazer? ");
+
             switch (acao) {
                 case "1":
+                    if (indice === null) {
+                        console.log(`Opção inválida`);
+                    }
+                    var remover = "1"
+                    var indice = carrinho[remover - 1].titulo;
+                    carrinho.splice(indice, 1);
+                    console.log(`Livro "${indice}" removido do carrinho.`);
+                    break;
+                case "2":
+                    if (indice === null) {
+                        console.log(`Opção inválida`);
+                    }
+                    var remover = "2"
+                    var indice = carrinho[remover - 1].titulo;
+                    carrinho.splice(indice, 1);
+                    console.log(`Livro "${indice}" removido do carrinho.`);
+                    break;
+                case "3":
+                    if (indice === null) {
+                        console.log(`Opção inválida`);
+                    }
+                    var remover = "3"
+                    var indice = carrinho[remover - 1].titulo;
+                    carrinho.splice(indice, 1);
+                    console.log(`Livro "${indice}" removido do carrinho.`);
+                    break;
+                case "8":
                     if(carrinho.length<3){
                         adicionarLivro()
                         break;
+                    } else {
+                        divisoria()
+                        console.log("Seu carrinho esta cheio!")
+                        divisoria()
+                        break;
                     }
-                    divisoria()
-                    console.log("Seu carrinho esta cheio!")
-                    divisoria()
-                    break;
-                case "2":
-                    removerLivro()
-                    break;
-                case "3":
-                    visualizarCarrinho()
+                case "9":
+                    console.log(`Fazendo checkout...`);
                     break;
                 case "0":
-                    console.log("Saindo do programa...");
+                    console.log(`Limpando carrinho...`);
+                    carrinho.pop();
+                    limparCarrinho.mostrarTela();
                     break;
                 default:
-                    console.log("Ação inválida. Por favor, escolha adicionar, remover, visualizar ou sair.");
+                    console.log("Ação inválida. Por favor, tente novamente!");
                     break;
             }
         }
 
 
         function adicionarLivro() {
-            // let escolha = prompt("Digite o número do livro que você deseja adicionar ao carrinho (ou 0 para sair):");
-        
-            while(carrinho.length < 3) {
-                escolha = prompt("Digite o número de outro livro que você deseja adicionar ao carrinho (ou 0 para sair):");
-                escolha = parseInt(escolha);
-                if (isNaN(escolha) || escolha < 1 || escolha > listaDeLivros.length) {
-                    console.log("Escolha inválida. Tente novamente.");
-                } else {
-                    carrinho.push(listaDeLivros[escolha - 1]);
-                    console.log(`Livro "${listaDeLivros[escolha - 1].titulo}" adicionado ao carrinho.`);
-                }
-            }
-
-
-        }
-        
-        function removerLivro() {
-
-            console.log(`              Meu Carrinho`);
-            console.log(` 1 → ${carrinho[0].titulo}`);
-            console.log(` 2 → ${carrinho[1].titulo}`);
-            console.log(` 3 → ${carrinho[2].titulo}`);
-
-            let remover = prompt("Qual livro você deseja remover?");
-
-            if (remover === "1" || remover === "2" || remover === "3") {
-                let indice = carrinho[remover - 1].titulo;
-                carrinho.splice(indice, 1);
-                console.log(`Livro "${indice}" removido do carrinho.`);
-                
+            escolha = prompt("Digite o número de outro livro que você deseja adicionar ao carrinho (ou 0 para sair):");
+            escolha = parseInt(escolha);
+            if (isNaN(escolha) || escolha < 1 || escolha > listaDeLivros.length) {
+                console.log("Escolha inválida. Tente novamente.");
             } else {
-                console.log(`Opção não encontrado no carrinho.`);
+                carrinho.push(listaDeLivros[escolha - 1]);
+                console.log(`Livro "${listaDeLivros[escolha - 1].titulo}" adicionado ao carrinho.`);
             }
         }
         
