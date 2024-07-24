60bede7d5a35eb78041c0686c2c2b8d57de51dda
Matheus Migge <matheusmigge@gmail.com>
2024-07-24 00:17:30 -0300

    navegação entre telas concluido

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const prompt = require("prompt-sync")();
 const divisoria = require("./elementosGraficos/divisoria");
 const boasVindas = require("./telas/boasVindas");
 const carrinhoObj = require("./telas/carrinho");  // Importando apenas o carrinhoObj
+const livrosConsulta = require("./telas/livrosConsulta");
 const login = require("./telas/login");
 const menuPrincipal = require("./telas/menuPrincipal");
 
@@ -14,38 +15,44 @@ login.mostrarTela();
 
 // Se o usuário for um pilantras, o programa avisará que há débitos a pagar
 
-// Menu Principal
-menuPrincipal.mostrarTela();
-
 // Se a opção selecionada não for válida (digitar um número que não seja 1, 2, 3, 4 ou 5) o programa avisará com um erro e voltará a pedir ao usuário a opção que ele deseja selecionar
 while (true) {
+  // Menu Principal
+  menuPrincipal.mostrarTela();
   let opcaoEscolhida = parseInt(prompt(" → "));
 
-  if (!isNaN(opcaoEscolhida) && opcaoEscolhida > 0 && opcaoEscolhida < 6) {
-    switch (opcaoEscolhida) {
-      case 1:
-        carrinhoObj.mostrarTela();
-        break;
+  switch (opcaoEscolhida) {
+    case 1:
 
-      case 2:
-        console.log(`Aqui fica a função de devolução / renovação`);
-        break;
+      if (login.contaLogada.debito > 0){
+        divisoria();
+        console.log('Atenção: você possui débitos em aberto em nosso sistema!');
+        console.log('Regularize sua situação assim que possível para poder voltar a usar o serviço de empréstimo de livros.');
+        divisoria();
+      } else {
+        livrosConsulta.mostrarTela();
+      }
+      break;
+    case 2:
+      console.log(`Aqui fica a função de devolução / renovação`);
+      break;
 
-      case 3:
-        console.log(`Aqui fica a função de quitação`);
-        break;
+    case 3:
+      console.log(`Aqui fica a função de quitação`);
+      break;
 
-      case 4:
-        console.log(`Aqui fica a função de informações`);
-        break;
+    case 4:
+      console.log(`Aqui fica a função de informações`);
+      break;
 
-      default:
-        console.log(`Encerrando programa...`);
-        process.exit();
-        break;
-    }
-    break;
-  } else {
-    console.log(`ERRO: Selecione uma opção válida!`);
+    case 5:
+      console.log(`Encerrando programa...`);
+      process.exit();
+      break;
+
+    default:
+      console.log(`ERRO: Selecione uma opção válida!`);
+      break;
   }
 }
+
